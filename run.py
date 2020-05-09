6d832edc981fda1b37d278fd0aa638962922f80c
Dark Tet <32560705+dark-tet@users.noreply.github.com>
2020-05-09 16:42:18 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def numberaccpect(message):
     message = message
     while True:
@@ -13,28 +12,26 @@ def numberaccpect(message):
         else:
             return userInput
             break
+
 _rankNum = numberaccpect('message')
 if _rankNum > 50:
     val = _rankNum
     int_val = val//50
     int_num = int_val*50
-    str_num = str(int_num)
-    mallink = 'https://myanimelist.net/topanime.php'
-    _linkreq = '?limit='
-    finalurl = (mallink+_linkreq+str_num)
+    finalurl = ('https://myanimelist.net/topanime.php?limit='+str(int_num))
 else:
     finalurl = 'https://myanimelist.net/topanime.php'
+
 website = requests.get(finalurl)
 soup = BeautifulSoup(website.text, 'lxml')
 for div_rank in soup.findAll('tr', class_='ranking-list'):
     rank = div_rank.td.span.text
-    ranknumber = rank
     div_name = div_rank.find('div', class_='di-ib clearfix')
     name = div_name.a.text
     global_score = div_rank.find('div', class_='js-top-ranking-score-col di-ib al')
     global_score = global_score.span.text
     anime_link = div_rank.find('a', class_='hoverinfo_trigger fl-l fs14 fw-b')['href']
-    if ranknumber == str(_rankNum):
+    if rank == str(_rankNum):
         print(rank+') '+name)
         print (global_score)
         print(anime_link)
