39c89b368926c60a3a050d727fcbe9a9b814bdb9
Dark Tet <32560705+dark-tet@users.noreply.github.com>
2020-05-09 16:21:56 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-_rankNum = input('import witch top anime yo want to see:-')
-if int(_rankNum) > 50:
-    val = int(_rankNum)
+
+
+def numberaccpect(message):
+    message = message
+    while True:
+        try:
+            userInput = int(input('Witch Rank Number You Want to See:- '))
+        except ValueError:
+            print('Please Import only NUMBER')
+            continue
+        else:
+            return userInput
+            break
+_rankNum = numberaccpect('message')
+str_rankNum = str(_rankNum)
+print(type(str_rankNum))
+if _rankNum > 50:
+    val = _rankNum
     int_val = val//50
     int_num = int_val*50
     str_num = str(int_num)
@@ -21,10 +36,7 @@ for div_rank in soup.findAll('tr', class_='ranking-list'):
     global_score = div_rank.find('div', class_='js-top-ranking-score-col di-ib al')
     global_score = global_score.span.text
     anime_link = div_rank.find('a', class_='hoverinfo_trigger fl-l fs14 fw-b')['href']
-    if ranknumber == _rankNum:
+    if ranknumber == str_rankNum:
         print(rank+') '+name)
         print (global_score)
         print(anime_link)
-
-
-
