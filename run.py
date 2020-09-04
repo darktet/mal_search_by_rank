f1a9cd812b9e838a972db5dc89cc0270a18f75d0
dark-vash <santobollove@gmail.com>
2020-09-04 15:41:47 +0600

    extra

--- run.py
+++ run.py
@@ -1,29 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-def rankUrl(userNumber=0, listType = 'anime'):
-    calculatUserNumber = ((userNumber - 1) // 50) * 50
-    if userNumber <= 50:
-        url = f'https://myanimelist.net/topanime.php?type={listType}'
+
+def rankUrl(animeType, rankNumber=0):
+    calculatRankNumber = ((rankNumber - 1) // 50) * 50
+    if rankNumber <= 50:
+        url = f'https://myanimelist.net/topanime.php?type={animeType}'
     else:
-        url = f'https://myanimelist.net/topanime.php?type={listType}&limit={calculatUserNumber}'     
+        url = f'https://myanimelist.net/topanime.php?type={animeType}&limit={calculatRankNumber}'
     return url
 
-def website(url):
-    website = requests.get(url)
-    soup = BeautifulSoup(website.text, 'lxml')
-    for div_rank in soup.findAll('tr', class_='ranking-list'):
-        rank = div_rank.td.span.text
-        div_name = div_rank.find('div', class_='di-ib clearfix')
-        name = div_name.a.text
-        global_score = div_rank.find(
-            'div', class_='js-top-ranking-score-col di-ib al')
-        global_score = global_score.span.text
-        anime_link = div_rank.find(
-            'a', class_='hoverinfo_trigger fl-l fs14 fw-b')['href']
-        print(rank+') '+name)
-        print(global_score)
-        print(anime_link)
-website(rankUrl())
 
+def website(animeType, rankNumber):
+    soup = BeautifulSoup(requests.get(
+        rankUrl(animeType, rankNumber)).text, 'lxml')
+    for trRank in soup.findAll('tr', class_='ranking-list'):
+        rank = trRank.td.span.text
+        animeName = trRank.find('div', class_='di-ib clearfix').a.text
+        imgLink = trRank.findAll('td')[1].a.img['data-src']
+        globalScore = trRank.find(
+            'div', class_='js-top-ranking-score-col di-ib al').span.text
+        animeLink = trRank.find(
+            'a', class_='hoverinfo_trigger fl-l fs14 fw-b')['href']
+        if rank == str(rankNumber):
+            return {'rank': rank, 'animeName': animeName,
+                    'globalScore': globalScore, 'animeLink': animeLink, 'imgLink': imgLink}
+
+
+print(website('anime', int(22)))
+print(website('upcoming', int(78)))
+print(website('movie', int(120)))
 
